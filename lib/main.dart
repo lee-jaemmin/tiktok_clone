6931bd510acba9969a304dff6948eb4484e456dc
lee-jaemmin <dlwoals8546@gmail.com>
2024-11-10 22:34:47 +0900

    pushtest

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const tiktok());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class tiktok extends StatelessWidget {
+  const tiktok({super.key});
 
   // This widget is the root of your application.
   @override
